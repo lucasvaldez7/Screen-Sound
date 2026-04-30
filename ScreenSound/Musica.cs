9e46b224f9d7ae8eba0c44e2ff4353188f70bc58
Lucas <lucass.valdez06@gmail.com>
2026-04-30 20:51:40 -0300

    👶 Nascimento do projeto

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenSound
+{
+      class Musica
+    {
+        public string nome;
+        public string artista;
+        public int duracao;
+        public bool disponivel;
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Artista: {artista}");
+            Console.WriteLine($"Duração: {duracao}");
+            Console.WriteLine($"Disponível: {disponivel}");
+
+        }
+    }
+}
