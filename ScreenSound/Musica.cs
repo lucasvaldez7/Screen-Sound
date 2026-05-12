0c4b4b3aab1a40b2728d98d83c26277538bf2b3f
Lucas <lucass.valdez06@gmail.com>
2026-05-12 20:45:14 -0300

    feat/adiciona centralizacao de acesso

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -4,19 +4,37 @@ using System.Text;
 
 namespace ScreenSound
 {
-      class Musica
+    class Musica
     {
         public string nome;
         public string artista;
         public int duracao;
-        public bool disponivel;
+        private bool disponivel;
 
+
+        public void EscreveDisponivel(bool value)
+        {
+            disponivel = value;
+
+        }
+        public bool LeDisponivel()
+        {
+            return disponivel;
+        }
         public void ExibirFichaTecnica()
         {
             Console.WriteLine($"Nome: {nome}");
             Console.WriteLine($"Artista: {artista}");
             Console.WriteLine($"Duração: {duracao}");
-            Console.WriteLine($"Disponível: {disponivel}");
+
+            if (disponivel)
+            {
+                Console.WriteLine("Disponível no plano.");
+            }
+            else
+            {
+                Console.WriteLine("Adquira o plano Plus+");
+            }
 
         }
     }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ScreenSound;
+
+Musica musica1 = new Musica();
+
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 1;
+musica1.EscreveDisponivel(true);
+Console.WriteLine(musica1.LeDisponivel());
+
+musica1.ExibirFichaTecnica();   
