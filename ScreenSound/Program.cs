0b93ff2e0ec1bc63195fcc7eda9fd1e800242158
Lucas <lucass.valdez06@gmail.com>
2026-05-15 21:17:22 -0300

    feat/Exibe Musicas do Album

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,11 +1,19 @@
 ﻿using ScreenSound;
 
+Album albumRadioHead = new Album();
+
+albumRadioHead.Nome = "OK Computer";
+
 Musica musica1 = new Musica();
+musica1.Nome = "Let Down";
+musica1.Duracao = 120;
 
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 1;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+Musica musica2 = new Musica();
+musica2.Nome = "Karma Police";
+musica2.Duracao = 150;
 
-musica1.ExibirFichaTecnica();   
+
+albumRadioHead.AdicionarMusica(musica1);
+albumRadioHead.AdicionarMusica(musica2);
+
+albumRadioHead.ExibirMusicasDoAlbum();
